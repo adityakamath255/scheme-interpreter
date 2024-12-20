a7f29c0c471761a3aba70599c5a508c1505bf7ec
adityakamath255 <aditya.kamath.270@gmail.com>
2024-12-20 17:12:01 +0530

    Add files via upload

---
+++ interface.hpp
@@ -0,0 +1,266 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <variant>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+namespace scheme {
+
+class symbol;
+class cons;
+class primitive;
+class procedure;
+class environment;
+class executor;
+class expression;
+
+typedef variant<
+  bool,
+  double,
+  symbol,
+  string,
+  cons*,
+  primitive*,
+  procedure*,
+  nullptr_t
+> sc_obj;
+
+struct symbol {
+  string name;
+
+  symbol(): name {"*undefined*"} {}
+
+  symbol(string s): name {s} {}
+
+  friend bool
+  operator==(const symbol& s0, const symbol& s1) {
+    return s0.name == s1.name;
+  }
+
+  friend bool
+  operator<(const symbol& s0, const symbol& s1) {
+    return s0.name < s1.name;
+  }
+};
+
+struct cons {
+  sc_obj car;
+  sc_obj cdr;
+  cons(sc_obj a, sc_obj b): car {a}, cdr {b} {}
+
+  sc_obj
+  operator[](const string& s) const {
+    sc_obj curr = const_cast<cons*>(this);
+
+    if (*s.begin() != 'c' || *s.rbegin() != 'r') {
+      throw runtime_error("invalid cons operation: " + s);
+      return nullptr;
+    }
+
+    for (int i = s.size() - 2; i > 0; i--) {
+      switch (s[i]) {
+        case 'a':
+          curr = get<cons*>(curr)->car;
+          break;
+        case 'd':
+          curr = get<cons*>(curr)->cdr;
+          break;
+        default:
+          throw runtime_error("invalid cons operation: " + s);
+          return nullptr;
+      }
+    }
+
+    return curr;
+  }
+
+  sc_obj
+  at(const string& s) const {
+    return operator[](s);
+  } 
+};
+
+struct primitive {
+  function<sc_obj(const vector<sc_obj>&)> func;
+  primitive(decltype(func) f): func {f} {}
+
+  sc_obj 
+  operator()(const vector<sc_obj>& args) const {
+    return func(args);
+  }
+};
+
+struct procedure {
+  const vector<symbol> parameters;
+  const executor *body;
+  environment *const env;
+
+  procedure(vector<symbol> p, executor *b, environment *e):
+    parameters {p},
+    body {b},
+    env {e}
+  {}
+};
+
+class environment {
+private:
+  map<symbol, sc_obj> frame {};
+  environment *const super;
+
+  map<symbol, sc_obj>::iterator
+  assoc(const symbol& s) {
+    const auto found = frame.find(s);
+    if (found != frame.end()) {
+      return found;
+    }
+    else if (super != nullptr) {
+      return super->assoc(s);
+    }
+    else {
+      throw runtime_error("Unbound variable: " + s.name);
+    }
+    return found;
+  }
+
+public:
+  environment():
+    super {nullptr} {}
+    
+  environment(environment *super_): 
+    super {super_} {}
+
+  void
+  set_variable(const symbol& s, const sc_obj obj) {
+    assoc(s)->second = obj;
+  }
+
+  sc_obj 
+  lookup(const symbol& s) {
+    return assoc(s)->second;
+  }
+
+  void
+  define_variable(const symbol& s, sc_obj obj) {
+    const auto found = frame.find(s);
+    if (found != frame.end()) {
+      throw runtime_error("binding already present: " + s.name);
+    }
+    else {
+      frame.insert({s, obj});
+    }
+  }
+};
+
+environment* 
+extend_env(const vector<symbol>& parameters, const vector<sc_obj>& arguments, environment *env) {
+  if (parameters.size() != arguments.size()) {
+    throw runtime_error("env extend size mismatch");
+    return nullptr; 
+  }
+  environment *ret = new environment(env);
+  for (int i = 0; i < parameters.size(); i++) {
+    ret->define_variable(parameters[i], arguments[i]);
+  }
+  return ret;
+}
+
+class executor {
+public:
+  virtual sc_obj
+  eval(environment*) const = 0;
+};
+
+class expression {
+public:
+  virtual executor*
+  analyze() const = 0;
+};
+
+bool
+is_pair(const sc_obj obj) {
+  return holds_alternative<cons*>(obj);
+}
+
+bool 
+is_null(const sc_obj obj) {
+  return holds_alternative<nullptr_t>(obj);
+}
+
+bool 
+is_true(const sc_obj obj) {
+  return (
+    !holds_alternative<bool>(obj) ||
+    get<bool>(obj) == true
+  );
+}
+
+bool
+is_false(const sc_obj obj) {
+  return !is_true(obj);
+}
+
+expression*
+classify(const sc_obj);
+
+sc_obj 
+eval(expression *expr, environment *const env) {
+  return expr->analyze()->eval(env);
+}
+
+sc_obj 
+apply(const sc_obj p, const vector<sc_obj>& args) {
+  if (holds_alternative<primitive*>(p)) {
+    const auto func = *get<primitive*>(p);
+    return func(args);
+  }
+  else if (holds_alternative<procedure*>(p)) {
+    const auto pp = get<procedure*>(p);
+    const auto new_env = extend_env(pp->parameters, args, pp->env);
+    return pp->body->eval(new_env);
+  }
+  else {
+    throw runtime_error("tried to apply an object that is not a procedure");
+    return nullptr;
+  }
+}
+
+vector<symbol>
+cons2symbols(sc_obj c) {
+  vector<symbol> ret;
+  while (is_pair(c)) {
+    const auto as_cons = get<cons*>(c);
+    ret.push_back(get<symbol>(as_cons->car));
+    c = as_cons->cdr;
+  }
+  return ret;
+}
+
+vector<expression*> 
+cons2vec(sc_obj seq) {
+  vector<expression*> vec {};
+  while (is_pair(seq)) {
+    const auto as_cons = get<cons*>(seq);
+    vec.push_back(classify(as_cons->car));
+    seq = as_cons->cdr; 
+  }
+  return vec;
+}
+
+vector<executor*> 
+exprs2execs(const vector<expression*>& arr) {
+  vector<executor*> ret {};
+  for (const auto expr : arr)
+    ret.push_back(expr->analyze());
+  return ret;
+}
+
+constexpr double precision = 1e-9;
+
+}
---
+++ primitives.cpp
@@ -0,0 +1,378 @@
+#include "interface.hpp"
+using namespace std;
+
+namespace scheme {
+
+void 
+display(const bool);
+
+void 
+display(const double);
+
+void 
+display(const symbol&);
+
+void 
+display(const string&);
+
+void 
+display(const nullptr_t);
+
+void 
+display(cons*);
+
+void 
+display(const bool b) {
+  cout << (b ? "#t" : "#f");
+}
+
+void 
+display(const double n) {
+  cout << n;
+}
+
+void 
+display(const symbol& s) {
+  cout << s.name;
+}
+
+void 
+display(const string& w) {
+  cout << "\"";
+  cout << w;
+  cout << "\"";
+}
+
+void 
+display(const nullptr_t x) {}
+
+void 
+display(const procedure *p) {
+  cout << "procedure at " << p;
+}
+
+void 
+display(const primitive *p) {
+  cout << "primitive at " << p;
+}
+
+struct {
+  template<typename T>
+  void 
+  operator()(T x) {
+    display(x);
+  }
+} display_overload;
+
+sc_obj
+display_iter(const sc_obj obj, const bool head) {
+  if (is_pair(obj)) {
+    if (!head)
+      cout << " ";
+    const auto c = get<cons*>(obj);
+    visit(display_overload, c->car);
+    return display_iter(c->cdr, 0);
+  }
+  else {
+    return obj;
+  }
+}
+
+void 
+display(cons *const c) {
+  cout << "(";
+  sc_obj obj = \
+  display_iter(c, 1);
+
+  if (!is_null(obj)) {
+    cout << " . ";
+    visit(display_overload, obj);
+  }
+  cout << ")";
+}
+
+void 
+display(const sc_obj obj) {
+  visit(display_overload, obj);
+}
+
+namespace prim_env {
+  sc_obj
+  display_final(const vector<sc_obj>& args) {
+    for (const auto& arg : args) {
+      display(arg);
+      cout << "\n";
+    }
+    return symbol("ok");
+  }
+
+  sc_obj
+  add(const vector<sc_obj>& args) {
+    double ret = 0.0;
+    for (const auto& arg : args) {
+      ret += get<double>(arg);
+    }
+    return ret;
+  }
+
+  sc_obj
+  sub(const vector<sc_obj>& args) {
+    if (args.size() == 1)
+      return -get<double>(args[0]);
+    else
+      return get<double>(args[0]) - get<double>(args[1]);   
+  }
+
+  sc_obj
+  mul(const vector<sc_obj>& args) {
+    double ret = 1.0;
+    for (const auto& arg : args) {
+      ret *= get<double>(arg);
+    }
+    return ret;
+  }
+
+  sc_obj
+  div(const vector<sc_obj>& args) {
+    return get<double>(args[0]) / get<double>(args[1]);
+  }
+
+  sc_obj 
+  exp(const vector<sc_obj>& args) {
+    return pow(get<double>(args[0]), get<double>(args[1]));
+  }
+
+  sc_obj 
+  remainder(const vector<sc_obj>& args) {
+    return fmod(get<double>(args[0]), get<double>(args[1]));
+  }
+
+  sc_obj
+  lt(const vector<sc_obj>& args) {
+    return get<double>(args[0]) < get<double>(args[1]);
+  }
+
+  sc_obj
+  gt(const vector<sc_obj>& args) {
+    return get<double>(args[0]) > get<double>(args[1]);
+  }
+
+  sc_obj 
+  eq(const vector<sc_obj>& args) {
+    return abs(get<double>(args[0]) - get<double>(args[1])) < precision;
+  }
+
+  sc_obj 
+  le(const vector<sc_obj>& args) {
+    return get<double>(args[0]) <= get<double>(args[1]);
+  }
+
+  sc_obj
+  ge(const vector<sc_obj>& args) {
+    return get<double>(args[0]) >= get<double>(args[1]);
+  }
+
+  sc_obj
+  neq(const vector<sc_obj>& args) {
+    return !get<bool>(eq(args));
+  }
+
+  sc_obj
+  is_equal(const vector<sc_obj>& args) {
+    return args[0] == args[1];
+  }
+
+  sc_obj
+  car(const vector<sc_obj>& args) {
+    return get<cons*>(args[0])->car;
+  }
+
+  sc_obj
+  cdr(const vector<sc_obj>& args) {
+    return get<cons*>(args[0])->cdr;
+  }
+
+  sc_obj
+  cons_fn(const vector<sc_obj>& args) {
+    return new cons(args[0], args[1]);
+  }
+
+  sc_obj
+  list_fn(const vector<sc_obj>& args) {
+    sc_obj ret = nullptr;
+    for (auto curr = args.rbegin(); curr != args.rend(); curr++) {
+      ret = new cons(*curr, ret);
+    }
+    return ret;
+  }
+
+  sc_obj
+  null_fn(const vector<sc_obj>& args) {
+    return holds_alternative<nullptr_t>(args[0]);
+  }
+
+  sc_obj
+  pair_fn(const vector<sc_obj>& args) {
+    return is_pair(args[0]);
+  }
+
+  sc_obj
+  id(const vector<sc_obj>& args) {
+    return args[0];
+  }
+
+  sc_obj
+  inc(const vector<sc_obj>& args) {
+    return 1 + get<double>(args[0]);
+  };
+
+  sc_obj
+  dec(const vector<sc_obj>& args) {
+    return -1 + get<double>(args[0]);
+  };
+
+  sc_obj
+  avg(const vector<sc_obj>& args) {
+    return get<double>(add(args)) / args.size();
+  };
+
+  sc_obj
+  sq(const vector<sc_obj>& args) {
+    return get<double>(args[0]) * get<double>(args[0]);
+  }
+
+  sc_obj
+  abs_fn(const vector<sc_obj>& args) {
+    return abs(get<double>(args[0]));
+  }
+
+  sc_obj
+  sin_fn(const vector<sc_obj>& args) {
+    return sin(get<double>(args[0]));
+  }
+
+  sc_obj
+  cos_fn(const vector<sc_obj>& args) {
+    return cos(get<double>(args[0]));
+  }
+
+  sc_obj
+  log_fn(const vector<sc_obj>& args) {
+    return log(get<double>(args[0]));
+  }
+
+  sc_obj
+  max_fn(const vector<sc_obj>& args) {
+    double ret = -INFINITY;
+    for (const auto& arg : args)
+      ret = max(ret, get<double>(arg));
+    return ret;
+  }
+
+  sc_obj
+  min_fn(const vector<sc_obj>& args) {
+    double ret = -INFINITY;
+    for (const auto& arg : args)
+      ret = min(ret, get<double>(arg));
+    return ret;
+  }
+
+  sc_obj
+  quotient(const vector<sc_obj>& args) {
+    return static_cast<double>(
+      static_cast<int>(
+        get<double>(args[0]) / 
+        get<double>(args[1])));
+  }
+
+  sc_obj
+  sqrt_fn(const vector<sc_obj>& args) {
+    return sqrt(get<double>(args[0]));
+  }
+
+  sc_obj
+  not_fn(const vector<sc_obj>& args) {
+    return !get<bool>(args[0]);
+  }
+
+  sc_obj
+  new_line(const vector<sc_obj>& args) {
+    cout << "\n";
+    return true;
+  }
+
+  sc_obj
+  list_len(const vector<sc_obj>& args) {
+    double ret = 0;
+    auto curr = args[0];
+    while (is_pair(curr)) {
+      ret++;
+      curr = get<cons*>(curr)->cdr;
+    }
+    return ret;
+  }
+
+  sc_obj
+  append_rec(const sc_obj list1, const sc_obj list2) {
+    if (is_pair(list1)) {
+      return new cons(
+        get<cons*>(list1)->car,
+        append_rec(get<cons*>(list1)->cdr, list2)
+      );
+    }
+    else {
+      return list2;
+    }
+  }
+
+  sc_obj
+  append(const vector<sc_obj>& args) {
+    return append_rec(args[0], args[1]);
+  }
+}
+
+using namespace prim_env;
+
+const vector<pair<string, function<sc_obj(const vector<sc_obj>&)>>> 
+prims = {
+  {"+", add},
+  {"-", sub},
+  {"*", mul},
+  {"/", div},
+  {"**", exp},
+  {"<", lt},
+  {">", gt},
+  {"=", eq},
+  {"<=", le},
+  {">=", ge},
+  {"!=", neq},
+  {"inc", inc},
+  {"dec", dec},
+  {"id", id},
+  {"avg", avg},
+  {"sq", sq},
+  {"eq?", is_equal},
+  {"equal?", is_equal},
+  {"car", car},
+  {"cdr", cdr},
+  {"cons", cons_fn},
+  {"list", list_fn},
+  {"null?", null_fn},
+  {"pair?", pair_fn},
+  {"abs", abs_fn},
+  {"sin", sin_fn},
+  {"cos", cos_fn},
+  {"log", log_fn},
+  {"max", max_fn},
+  {"min", min_fn},
+  {"expt", exp},
+  {"quotient", quotient},
+  {"remainder", remainder},
+  {"sqrt", sqrt_fn},
+  {"newline", new_line},
+  {"display", display_final},
+  {"length", list_len},
+  {"append", append}
+};
+
+}
