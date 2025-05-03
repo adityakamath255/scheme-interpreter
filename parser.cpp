5864cd67d4623d0376add18e1c2d7f1a2435097c
adityakamath255 <aditya.kamath.270@gmail.com>
2025-05-03 17:29:39 +0530

    better parser

--- parser.cpp
+++ parser.cpp
@@ -5,121 +5,176 @@
 
 namespace Scheme {
 
-Obj
-Parser::make_num_obj(const std::string_view str) {
-  double val;
-  auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), val);
-  if (ec != std::errc()) {
-    throw std::runtime_error("failed to parse number");
-  }
-  return Obj(val);
-}
-
-Obj
-Parser::make_sym_obj(const std::string_view str) {
-  return Obj(interp.intern_symbol(str));
-}
-
-Obj
-Parser::make_bool_obj(const std::string_view str) {
-  if (str[1] == 't')
-    return Obj(true);
-  else if (str[1] == 'f') 
-    return Obj(false);
-  else 
-    return make_sym_obj(str);
-}
-
-Obj
-Parser::make_str_obj(const std::string_view str) {
-  return Obj(std::string(str.data() + 1, str.size() - 2));
-}
-
-Obj
-Parser::from_str(const std::string_view str) {
-  if (str[0] == '#') {
-    return make_bool_obj(str);
-  }
-  else if (str[0] == '"') {
-    return make_str_obj(str);
-  }
-  else if (str[0] == '-') {
-    if (str.size() != 1 && isdigit(str[1]))
-      return make_num_obj(str);
-    else
-      return make_sym_obj(str);
-  }
-  else if (isdigit(str[0])) {
-      return make_num_obj(str);
-  } 
-  else {
-    return make_sym_obj(str);
-  }
-} 
-
-Obj
-Parser::parse_vec() {
-  std::vector<Obj> ret {};
-  while (index < tokens.size() && tokens[index] != ")") {
-    ret.push_back(parse_impl(false));
-  }
-  if (index == tokens.size()) {
-    throw std::runtime_error("unterminated vector");
-  }
-  else {
-    index++;
-    return interp.spawn<Vector>(std::move(ret));
-  }
-}
-
 Parser::Parser(const std::vector<std::string_view>& tokens, Interpreter& interp):
   tokens {tokens},
   index {0},
   interp {interp}
 {}
 
-Obj
-Parser::parse_impl(bool recursive) {
-  const auto token = tokens[index];
-  Obj head;
+std::string_view 
+Parser::next_token() {
+  if (index >= tokens.size()) {
+    return "\0"; // replace with Token::EOF
+  }
+  else {
+    index++;
+    return tokens[index - 1];
+  }
+}
 
-  index++;
+bool
+Parser::match(const std::string_view str) {
+  if (index >= tokens.size()) {
+    return false;
+  }
+  else if (tokens[index] == str) {
+    index++;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+Obj
+Parser::make_sym_obj(const std::string_view str) {
+  return interp.intern_symbol(str);
+}
+
+Obj
+Parser::make_bool_obj(const std::string_view str) {
+  if (str.size() != 2) {
+    throw std::runtime_error("identifiers cannot start with '#'");
+  }
+  switch (str[1]) {
+    case 't': 
+    case 'T':
+      return true;
+
+    case 'f':
+    case 'F':
+      return false;
+
+    default:
+      throw std::runtime_error("identifiers cannot start with '#'");
+  }
+}
+
+Obj
+Parser::make_num_obj(const std::string_view str) {
+  try {
+    size_t chars_processed;
+    const double val = std::stod(std::string(str), &chars_processed);
+
+    if (chars_processed == str.size()) {
+      return val;
+    }
+    else {
+      return make_sym_obj(str);
+    }
+  }
+  catch (const std::exception&) {
+    return make_sym_obj(str);
+  }
+}
+
+Obj
+Parser::make_str_obj(const std::string_view str) {
+  return std::string(str.data() + 1, str.size() - 2);
+}
+
+Obj
+Parser::from_str(const std::string_view str) {
+  switch (str[0]) {
+    case '#':
+      return make_bool_obj(str);
+    case '"':
+      return make_str_obj(str);
+    case '0': case '1': case '2': case '3': case '4': 
+    case '5': case '6': case '7': case '8': case '9':
+    case '+': case '-':
+      return make_num_obj(str);
+    default:
+      return make_sym_obj(str);
+  }
+} 
+
+Obj 
+Parser::parse_atom() {
+  const auto token = next_token();
 
   if (token == ")") {
     return nullptr;
   }
   else if (token == "(") {
-    head = parse_impl(true);
+    return parse_list();
   }
   else if (token == "#(") {
-    head = parse_vec();
+    return parse_vec();
   }
   else if (token == ".") {
-    head = parse_impl(false);
-    if (index >= tokens.size() || tokens[index] != ")") {
-      throw std::runtime_error("bad positioning of '.'");
-    }
-    index++;
-    recursive = false;
+    return parse_dotted_tail();
   }
   else if (token == "'") {
-    auto quoted = parse_impl(false);
-    head = Obj(interp.spawn<Cons>(
-      make_sym_obj("quote"), interp.spawn<Cons>(
-      quoted, 
-      nullptr)));
+    return parse_quoted("quote");
+  }
+  else if (token == "`") {
+    return parse_quoted("quasiquote");
+  }
+  else if (token == ",") {
+    return parse_quoted("unquote");
+  }
+  else if (token == ",@") {
+    return parse_quoted("unquote-splicing");
   }
   else {
-    head = from_str(token);
+    return from_str(token);
   }
+}
 
-  if (index == tokens.size() || !recursive) {
-    return head;
+Obj
+Parser::parse_list() {
+  if (match(")")) {
+    return nullptr;
+  }
+  if (match(".")) {
+    return parse_dotted_tail();
   }
   else {
-    auto tail = parse_impl(true);
+    const auto head = parse_atom();
+    const auto tail = parse_list();
     return Obj(interp.spawn<Cons>(head, tail));
   }
+}
+
+Obj
+Parser::parse_vec() {
+  std::vector<Obj> ret {};
+  while (!match(")")) {
+    ret.push_back(parse_atom());
+  }
+  return interp.spawn<Vector>(std::move(ret));
+}
+
+Obj 
+Parser::parse_dotted_tail() {
+  if (match(")")) {
+    throw std::runtime_error("bad positioning of '.'");
+  }
+  const auto ret = parse_atom();
+  if (!match(")")) {
+    throw std::runtime_error("bad positioning of '.'");
+  }
+  return ret;
+}
+
+Obj
+Parser::parse_quoted(const std::string_view quote_type) {
+  const auto quoted = parse_atom();
+  return Obj(interp.spawn<Cons>(
+    make_sym_obj(quote_type), interp.spawn<Cons>(
+    quoted, 
+    nullptr)));
 }
 
 Obj
@@ -127,7 +182,7 @@ Parser::parse() {
   if (tokens.empty()) {
     return Void {};
   }
-  return parse_impl(false);
+  return parse_atom();
 }
 
 }
--- parser.hpp
+++ parser.hpp
@@ -11,13 +11,18 @@ private:
   size_t index;
   Interpreter& interp;
 
+  std::string_view next_token();
+  bool match(const std::string_view);
   Obj make_num_obj(const std::string_view);
   Obj make_sym_obj(const std::string_view);
   Obj make_bool_obj(const std::string_view);
   Obj make_str_obj(const std::string_view);
   Obj from_str(const std::string_view);
+  Obj parse_atom();
+  Obj parse_list();
   Obj parse_vec();
-  Obj parse_impl(bool);
+  Obj parse_dotted_tail();
+  Obj parse_quoted(const std::string_view);
 
 public:
   Parser(const std::vector<std::string_view>&, Interpreter&);
